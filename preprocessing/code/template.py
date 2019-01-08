d596433a387711d479092236ac2bfcfb02f41933
卫宫 <wuyeran@qiandongdong.cn>
2019-01-08 11:03:39 +0800

    init

---
+++ preprocessing/code/template.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 14 16:14:20 2018
+
+@author: carey
+"""
+
+#import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+data_path = '../data/Data.csv'
+
+#import the dataset
+dataset = pd.read_csv(data_path)
+X = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,3].values
+
+# Taking care of missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+# Encoding categorical data
+# Encoding the Independent Variable
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+# Encoding the Dependent Variable
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
+
+#spliting the dataset to trainset and testset
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+#feature scaling
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
---
+++ regression/code/simple.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: carey
+"""
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+
+data_path = '../simple_regression/data/Salary_Data.csv'
+
+dataset = pd.read_csv(data_path)
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 1].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1/3, random_state=0)
+
+# Fitting Simple Linear Regression to the Training Set
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = regressor.predict(X_test)
+
+#Visualising the Training set results
+plt.scatter(X_train, y_train, c='red')
+plt.plot(X_train, regressor.predict(X_train), c='blue')
+plt.title('Salary VS Experience (training set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+#Visualising the test set results
+plt.scatter(X_test, y_test, c='red')
+plt.plot(X_train, regressor.predict(X_train), c='blue')
+plt.title('Salary VS Experience (test set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
